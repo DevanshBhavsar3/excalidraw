331f1b3e48bca67a7661a9204804d20ec428e671
Devansh Bhavsar <devanshbhavsar2006@gmail.com>
2025-08-10 13:59:00 +0530

    updated http backend url

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,9 +1,9 @@
 export const HTTP_URL =
   process.env.NODE_ENV === "production"
-    ? "http://drawifyapi.devanshbhavsar.tech"
+    ? "https://drawifyapi.devanshbhavsar.tech"
     : "http://localhost:3001";
 
 export const WS_URL =
   process.env.NODE_ENV === "production"
-    ? "http://drawifyws.devanshbhavsar.tech"
+    ? "https://drawifyws.devanshbhavsar.tech"
     : "ws://localhost:8080";
